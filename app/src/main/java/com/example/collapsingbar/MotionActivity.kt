c9ed246f8e15f81394704f8b4393ff122112f6fc
ShodiyorXurramov <xurramoshodiyor377@gmail.com>
2022-02-15 18:49:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/collapsingbar/MotionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.collapsingbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.jghvjh)
+    }
+}
